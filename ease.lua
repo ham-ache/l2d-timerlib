a51d1f3dd7d071e3b6c340cf197a28c87d0e5184
hamache <kikimosbon@gmail.com>
2025-05-02 23:05:53 +0200

    remove duplicating check in ease

--- ease.lua
+++ ease.lua
@@ -11,77 +11,69 @@ local c3 = c1 + 1
 local c4 = (2*pi)/3
 local c5 = (2*pi)/4.5
 
+local function exitwrap(f)
+    return function(t, ...)
+        if t == 0 and t == 1 then return end
+        return f(t, ...)
+    end
+end
+
 local ease = setmetatable({
     sine = {
-        i = function(t)
-            if t == 0 or t == 1 then return t end
+        i = exitwrap(function(t)
             return 1 - c((t*pi)/2)
-        end,
-        o = function(t)
-            if t == 0 or t == 1 then return t end
+        end),
+        o = exitwrap(function(t)
             return s((t*pi)/2)
-        end,
-        io = function(t)
-            if t == 0 or t == 1 then return t end
+        end),
+        io = exitwrap(function(t)
             return -(c(pi*t)-1)/2
-        end
+        end)
     },
     anyexp = {
-        i = function(t, exp)
-            if t == 0 or t == 1 then return t end
+        i = exitwrap(function(t, exp)
             return t^exp
-        end,
-        o = function(t, exp)
-            if t == 0 or t == 1 then return t end
+        end),
+        o = exitwrap(function(t, exp)
             return 1 - (1-t)^exp
-        end,
-        io = function(t, exp)
-            if t == 0 or t == 1 then return t end
+        end),
+        io = exitwrap(function(t, exp)
             return t < 0.5  and  2^(exp-1) * t^exp  or  1 - (-2*t+2)^exp/2
-        end
+        end)
     },
     circ = {
-        i = function(t)
-            if t == 0 or t == 1 then return t end
+        i = exitwrap(function(t)
             return 1 - r(1-t^2)
-        end,
-        o = function(t)
-            if t == 0 or t == 1 then return t end
+        end),
+        o = exitwrap(function(t)
             return r(1-(t-1)^2)
-        end,
-        io = function(t)
-            if t == 0 or t == 1 then return t end
+        end),
+        io = exitwrap(function(t)
             return (t < 0.5  and  1-r(1-(2*t)^2)  or  r(1-(-2*t+2)^2) + 1)/2
-        end
+        end)
     },
     elastic = {
-        i = function(t)
-            if t == 0 or t == 1 then return t end
+        i = exitwrap(function(t)
             return -2^(10*t-10) * s((t*10-10.75)*c4)
-        end,
-        o = function(t)
-            if t == 0 or t == 1 then return t end
+        end),
+        o = exitwrap(function(t)
             return 2^(-10*t) * s((t*10-0.75)*c4) + 1
-        end,
-        io = function(t)
-            if t == 0 or t == 1 then return t end
+        end),
+        io = exitwrap(function(t)
             local sin = s((20*t-11.125)*c5)/2
             return t < 0.5  and  -2^(20*t-10) * sin  or  2^(-20*t+10) * sin + 1
-        end
+        end)
     },
     back = {
-        i = function(t)
-            if t == 0 or t == 1 then return t end
+        i = exitwrap(function(t)
             return c3 * t^3 - c1 * t^2
-        end,
-        o = function(t)
-            if t == 0 or t == 1 then return t end
+        end),
+        o = exitwrap(function(t)
             return 1 + c3 * (t-1)^3 + c1 * (t-1)^2
-        end,
-        io = function(t)
-            if t == 0 or t == 1 then return t end
+        end),
+        io = exitwrap(function(t)
             return t < 0.5  and  (2*t)^2 * ((c2+1)*2*t-c2)/2  or  ((2*t-2)^2*((c2+1)*(t*2-2)+c2)+2)/2
-        end
+        end)
     },
     lerp = function(t, t2, f)
         return t + f*(t2-t)
